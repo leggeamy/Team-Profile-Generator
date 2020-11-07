923b57f82ff95836a36696d8702eca89ed7340c3
Amy Legge <amylegge@Amys-MacBook-Pro.local>
2020-11-07 10:39:50 -0500

    passing tests for Employee properties

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -6,13 +6,29 @@ test("Can make a new employee instance", () => {
     const e = new Employee();
     expect(typeof(e)).toBe("object");
 });
-
+// *Properties
 // can set employee name
+test("Can set employee name", () => {
+    const name= "Alex";
+    const e = new Employee(name);
+    expect(e.name).toBe(name);
+});
 
 // can set employee ID
+test("Can set employee id", () => {
+    const testValue = 100;
+    const e = new Employee("Foo", testValue);
+    expect(e.id).toBe(testValue);
+});
 
 // can set employee email
+test("Can set up employee email", () => {
+    const testValue = "test@test.com";
+    const e = new Employee("Foo", 1, testValue);
+    expect(e.email).toBe(testValue);
+});
 
+// *Methods
 // can get name with getName()
 
 // can get ID with getId()
