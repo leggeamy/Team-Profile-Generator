f7aa9eff8e60643d538aeb1a564af0d460e1e42b
Amy Legge <amylegge@Amys-MacBook-Pro.local>
2020-11-08 16:36:43 -0500

    final passing tests for intern

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,2 +1,24 @@
-// const Intern = require("../lib/Intern");
+const Intern = require("../lib/Intern");
+
+//can set school for new Intern
+test("can set school for new intern", () => {
+const testValue = "NYU";
+const e = new Intern("Foo", 1, "test@test.com", testValue);
+expect(e.school).toBe(testValue);
+});
+
+//getRole() should return "intern"
+test("getRole() should return 'Intern'", () => {
+    const testValue = "Intern";
+    const e = new Intern ("Foo", 1, "test@test.com", "NYU");
+    expect(e.getRole()).toBe(testValue);
+});
+
+//can get school name with getSchool()
+test("can get school name with getSchool()", () => {
+    const testValue = "NYU";
+    const e = new Intern ("Foo", 1, "test@test.com", testValue);
+    expect(e.getSchool()).toBe(testValue);
+});
+
 
