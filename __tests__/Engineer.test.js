92a4e608b12e5c7261f17d9d2e910853a5de22c6
Amy Legge <amylegge@Amys-MacBook-Pro.local>
2020-11-08 16:24:13 -0500

    passing tests for Engineer

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,7 +1,22 @@
 const Engineer = require("../lib/Engineer");
 
 //can set GitHub account for new Engineer
+test("Can set GitHub account for new Engineer", () => {
+    const testValue = "GitHubUser";
+    const e = new Engineer("Foo", 1, "test@test.com", testValue);
+    expect(e.github).toBe(testValue);
+});
 
 //getRole() should return "Engineer"
+test("getRole() should return \"Engineer\"", () => {
+    const testValue = "Engineer";
+    const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
+    expect(e.getRole()).toBe(testValue);
+})
 
-//can get getGithub username with getGithub
+//can get get GitHub username with getGithub
+test("can get GitHub username with getGithub()", () => {
+    const testValue = "GitHubUser";
+    const e = new Engineer("Foo", 1, "test@test.com", testValue);
+    expect(e.getGithub()).toBe(testValue);
+});
