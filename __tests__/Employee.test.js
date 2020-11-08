2c5f89cbfe9888486fde3e2fa9b8fa5f735abc0a
Amy Legge <amylegge@Amys-MacBook-Pro.local>
2020-11-08 15:02:56 -0500

    passing all Employee tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,12 +1,13 @@
 const Employee = require("../lib/Employee");
-const { TestScheduler } = require("jest");
 
 // can make a new employee instance (object)
 test("Can make a new employee instance", () => {
     const e = new Employee();
     expect(typeof(e)).toBe("object");
 });
-// *Properties
+
+// *Testing Properties
+
 // can set employee name
 test("Can set employee name", () => {
     const name= "Alex";
@@ -28,11 +29,32 @@ test("Can set up employee email", () => {
     expect(e.email).toBe(testValue);
 });
 
-// *Methods
+// *Testing Methods
+
 // can get name with getName()
+test("can get name with getName()", () => {
+const testValue = "Alex";
+const e = new Employee(testValue);
+expect(e.getName()).toBe(testValue);
+});
 
 // can get ID with getId()
+test("can get id with getId()", () => {
+    const testValue = 100;
+    const e = new Employee("Foo", testValue);
+    expect(e.getId()).toBe(testValue);
+});
 
 // can get email with getEmail()
+test("can get email with getEmail()", () => {
+    const testValue = "test@test.com";
+    const e = new Employee("Foo", 1, testValue);
+    expect(e.getEmail()).toBe(testValue);
+});
 
 // can get role with getRole()
+test("getRole() should return \"Employee\"", () => {
+    const testValue = "Employee";
+    const e = new Employee("Foo, 1, test@test.com");
+    expect(e.getRole()).toBe(testValue);
+});
