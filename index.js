633d98f4a06af50e888dcfe10c8e0bac966514a8
Amy Legge <amylegge@Amys-MacBook-Pro.local>
2020-11-08 22:39:07 -0500

    correcting error in output path

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require("path");
 const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputpath = path.join(OUTPUT_DIR, "team.html");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
